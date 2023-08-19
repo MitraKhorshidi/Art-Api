fc977debfa215c4655507ef2c19f231ecb22fcee
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-19 13:17:40 +0200

    addCollectionCard

---
+++ src/components/Home/CollectionCard.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const CollectionCard = () => {
+  return (
+    <Link href='/'>
+      <img/>
+      <p>artist</p>
+      <h3>art title</h3>
+    </Link>
+  )
+}
+
+export default CollectionCard

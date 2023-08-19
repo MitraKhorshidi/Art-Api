a5bef1017b8887d3e45061858ef1c5093de0cbd0
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-19 13:18:08 +0200

    add Detail

---
+++ src/app/single/[id]/Detail.tsx
@@ -0,0 +1,32 @@
+
+export const Detail = () => {
+  return (
+    <div className="flex flex-col justify-center">
+        <div>
+            <img/>
+            <h2>artcraft title</h2>
+        </div>
+        <div className="flex flex-col justify-start">
+            <p>Title</p>
+            <h3>title</h3>
+            <hr/>
+
+            <p>Artist</p>
+            <h3>artsit</h3>
+            <hr/>
+
+            <p>Object Type</p>
+            <h3>type</h3>
+            <hr/>
+
+            <p>Measurments</p>
+            <h3>measurs</h3>
+            <hr/>
+
+            <p>Description</p>
+            <h3>desc</h3>
+            
+        </div>
+    </div>
+  )
+}

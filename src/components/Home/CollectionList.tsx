82ffe3bbf3300047218a1c6cb38d1167fc27b2e5
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-19 13:17:24 +0200

    add CollectionList

---
+++ src/components/Home/CollectionList.tsx
@@ -0,0 +1,13 @@
+import CollectionCard from "./CollectionCard";
+
+const CollectionList = () => {
+  return (
+    <div>
+      <h1>Title</h1>
+      list of cards
+      <CollectionCard />
+    </div>
+  );
+};
+
+export default CollectionList;

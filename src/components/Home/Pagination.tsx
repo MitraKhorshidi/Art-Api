134f4e6c762827ab9752d440bcc238b5b5e7bb53
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-20 20:37:22 +0200

    fix pagination

--- src/components/Home/Pagination.tsx
+++ src/components/Home/Pagination.tsx
@@ -9,7 +9,7 @@ const Pagination = ({
   searchParams: FilterProps;
 }) => {
 
-  const currentPage = searchParams.page || 1;
+  const currentPage = Number(searchParams.page || 1);
   if (currentPage === 1) {
     return (
       <div className="flex flex-row justify-end gap-3 my-5">

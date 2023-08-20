b68e14ba3ed1da891499009b734dad2342f89a53
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-20 20:38:56 +0200

    implement search bar

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -1,12 +1,29 @@
-import Button from "../Button"
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { generateLink } from "@/utils/links";
 
 const SearchBar = () => {
+  // router.push(newPathName ,{scroll:false});
+  const [searchInput, setSearchInput] = useState("");
+  const router = useRouter();
+
+  function searchHandler(){
+    const link = generateLink({page:1 , query:searchInput});
+    router.push(link);
+  }
   return (
     <div className="flex flex-row justify-center gap-4">
-        <input className="bg-[#1F1F1F66]  text-[#FFFFFF26] border border-1 border-[#FFFFFF26] rounded-[80px] py-2 px-6 gap-6"/>
-        <Button/>
+      <input
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        placeholder="Please type in your search"
+        className="bg-[#1F1F1F66]  text-[#FFFFFF26] border border-1 border-[#FFFFFF26] rounded-[80px] py-2 px-6 gap-6"
+      />
+      <button onClick={searchHandler}>Search</button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;

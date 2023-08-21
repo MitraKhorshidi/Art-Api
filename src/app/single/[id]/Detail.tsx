3e9723ab026935334eacc98971f5a0276aaae1d7
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-21 15:55:04 +0200

    make single page responsive

--- src/app/single/[id]/Detail.tsx
+++ src/app/single/[id]/Detail.tsx
@@ -17,7 +17,7 @@ export const Detail = ({ details }: { details: ArtObjectDetails }) => {
           className="relative w-full rounded-lg bg-cover aspect-[1408/850]"
           style={{ backgroundImage: `url(${webImage.url})` }}
         >
-          <h1 className="absolute left-[2.35%] bottom-[4%] w-[1043px] aspect-[1043/64] text-[56px] leading-[64px] text-primary shadow-[0px_4px_4px_0px_rgba(0, 0, 0, 0.06)]">
+          <h1 className="absolute left-[2%] bottom-[4%] w-[90%] aspect-[1043/64] text-2xl sm:text-[56px] leading-8 sm:leading-[64px] text-primary shadow-[0px_4px_4px_0px_rgba(0, 0, 0, 0.06)]">
             {longTitle}
           </h1>
         </div>
@@ -26,28 +26,28 @@ export const Detail = ({ details }: { details: ArtObjectDetails }) => {
         {longTitle && (
           <div className="gap-1 pt-2 pb-4 text-secondary border-b-[1px] border-b-secondary">
             <p className="text-base leading-6">Title</p>
-            <h3 className="text-2xl text-primary leading-8 ">{longTitle}</h3>
+            <h3 className="text-lg sm:text-2xl text-primary leading-8 ">{longTitle}</h3>
           </div>
         )}
 
         {principalMaker && (
           <div className="gap-1 pt-2 pb-4 text-secondary border-b-[1px] border-b-secondary">
             <p className="text-base leading-6">Artist</p>
-            <h3 className="text-2xl text-primary leading-8 ">{principalMaker}</h3>
+            <h3 className="text-lg sm:text-2xl text-primary leading-8 ">{principalMaker}</h3>
           </div>
         )}
 
         {objectTypes && (
           <div className="gap-1 pt-2 pb-4 text-secondary border-b-[1px] border-b-secondary">
             <p className="text-base leading-6">Object Type</p>
-            <h3 className="text-2xl text-primary leading-8 ">{objectTypes}</h3>
+            <h3 className="text-lg sm:text-2xl text-primary leading-8 ">{objectTypes}</h3>
           </div>
         )}
 
         {dimensions.length > 0 && (
           <div className="gap-1 pt-2 pb-4 text-secondary border-b-[1px] border-b-secondary">
             <p className="text-base leading-6">Measurments</p>
-            <h3 className="text-2xl text-primary leading-8 ">
+            <h3 className="text-lg sm:text-2xl text-primary leading-8 ">
               {dimensions.map((obj) => (
                 <span key={obj.type}>
                   {" "}
@@ -61,7 +61,7 @@ export const Detail = ({ details }: { details: ArtObjectDetails }) => {
         {description && (
           <div className="gap-1 pt-2 pb-4 text-secondary border-b-[1px] border-b-secondary">
             <p className="text-base leading-6">Description</p>
-            <h3 className="text-2xl text-primary leading-8 ">{description}</h3>
+            <h3 className="text-lg sm:text-2xl text-primary leading-8 ">{description}</h3>
           </div>
         )}
       </div>

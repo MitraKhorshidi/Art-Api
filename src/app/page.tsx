60b7c237f10dc3a7638870a276b4a12ae1436c3e
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-21 18:11:27 +0200

    clean collection title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { HomeProps ,FilterProps } from "@/data/types";
+import { HomeProps, FilterProps } from "@/data/types";
 import { getArtcraftsList } from "@/data/api";
 import CollectionList from "@/components/Home/CollectionList";
 import Pagination from "@/components/Home/Pagination";
@@ -6,19 +6,36 @@ import NavBar from "@/components/layout/NavBar";
 import Footer from "@/components/layout/Footer";
 
 export default async function Home({ searchParams }: HomeProps) {
-  const { objects: artCraftsList, pageCount: lastPage ,totlaCount:totlaCount} =
-    await getArtcraftsList(searchParams);
-  return (<>
-    <NavBar  searchParams={searchParams}/>
-    <main className="flex flex-col items-centr justify-center gap-8 flex-1">
-      {artCraftsList && (
-        <CollectionList list={artCraftsList} mainTitle={searchParams.query || "All artworks"  } searchResultTitle={searchParams.query ? `Found ${totlaCount} results for :` : undefined}/>
-      )}
-      <Pagination searchParams={searchParams} lastPage={lastPage} />
-    </main>
-    <Footer />
-  </>);
+  const {
+    objects: artCraftsList,
+    pageCount: lastPage,
+    totlaCount: totlaCount,
+  } = await getArtcraftsList(searchParams);
+
+  const isInSearch = searchParams && searchParams.query;
+
+  return (
+    <>
+      <NavBar searchParams={searchParams} />
+
+      <main className="flex flex-col items-centr justify-center gap-8 flex-1">
+          <h1 className="text-[56px] leading-[64px]">
+            {!isInSearch ? (
+              <span className="text-secondary">All artworks</span>
+            ) : (
+              <>
+                <span className="text-secondary">{`Found ${totlaCount} results for:`}</span>
+                <span className="text-primary"> {`${searchParams.query}`}</span>
+              </>
+            )}
+          </h1>
+
+        <CollectionList  list={artCraftsList} />
+
+        <Pagination searchParams={searchParams} lastPage={lastPage} />
+      </main>
+
+      <Footer />
+    </>
+  );
 }
-
-
-
--- src/components/Home/CollectionList.tsx
+++ src/components/Home/CollectionList.tsx
@@ -3,20 +3,13 @@ import CollectionCard from "./CollectionCard";
 
 const CollectionList = ({
   list,
-  mainTitle,
-  searchResultTitle,
 }: {
   list: ArtObject[],
-  mainTitle: string,
-  searchResultTitle? : string,
 }) => {
+
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row text-[56px] leading-[64px] mb-8">
-        <h1 className="text-secondary">{searchResultTitle}</h1>
-        <h1 className={searchResultTitle ? 'text-primary' : 'text-secondary'}>{mainTitle}</h1>
-      </div>
-      <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-10">
+    <div className="flex flex-col  flex-1">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {list.map((item) => (
           <CollectionCard
             key={item.id}

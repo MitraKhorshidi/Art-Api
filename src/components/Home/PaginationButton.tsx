b699a1e886280153177c0fedfa42d33e73862a8d
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-08-21 20:05:59 +0200

    clean pagination

--- src/components/Home/PaginationButton.tsx
+++ src/components/Home/PaginationButton.tsx
@@ -14,8 +14,8 @@ const PaginationButton = ({
   className?: string;
 }) => {
   let classes = !isActive
-    ? " flex items-center justify-center bg-bg-primary text-secondary text-center text-[18px] leading-6 bg-parimary rounded-full   w-[56px] h-[56px] aspect-[56/56] gap-[10px] border border-1 border-[#333333] shadow-[0px_16px_16px_4px_rgba(0, 0, 0, 0.01)] "
-    : " flex items-center justify-center bg-bg-primary text-secondary text-center text-[18px] leading-6 bg-parimary rounded-full  w-[56px] h-[56px] aspect-[56/56] gap-[10px] border border-1 border-btn-primary shadow-[0px_16px_16px_4px_rgba(0, 0, 0, 0.01)] ";
+    ? " flex items-center justify-center bg-bg-primary text-secondary text-center text-[18px] leading-6 bg-parimary rounded-full   w-[56px] h-[56px]  gap-[10px] border border-1 border-[#333333] shadow-[0px_16px_16px_4px_rgba(0, 0, 0, 0.01)] "
+    : " flex items-center justify-center bg-bg-primary text-secondary text-center text-[18px] leading-6 bg-parimary rounded-full  w-[56px] h-[56px]  gap-[10px] border border-1 border-btn-primary shadow-[0px_16px_16px_4px_rgba(0, 0, 0, 0.01)] ";
 
   if (className) classes = classes + className;
   if (!searchParams) {
